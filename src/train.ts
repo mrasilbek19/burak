60bb1af9baf3ab2f03a80c8ceb8426a3d5ae5464
mrasilbek19 <asilbek190303@gmail.com>
2026-05-26 14:22:33 +0900

    feat: express integraton & Task M

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+//task M
+function numberSquare(n: number[]) {
+    const result = n.map((num) => ({
+        number: num,
+        square: num ** 2,
+    }));
+    return result;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+console.log(numberSquare(numbers));
--- train.py
+++ train.py
@@ -1,13 +1,26 @@
-def reverseSentence(str):
-    str = str.lower()
-    list1 = str.split(" ")
-    print(list1)
-    reversed_words = []
-    for word in list1:
-        reversed_words.append(word[::-1])
-
-    return " <--> ".join(reversed_words)
+# TASK M
+def numberSquare(n):
+    return list(map(lambda num: {
+        "number": num,
+        "square": num ** 2
+    }, n))
 
 
-print(reverseSentence("we like coding!"))
-print(reverseSentence("Burak project"))
+numbers = list(num for num in range(10))
+
+print(numberSquare(numbers))
+
+
+# def reverseSentence(str):
+#     str = str.lower()
+#     list1 = str.split(" ")
+#     print(list1)
+#     reversed_words = []
+#     for word in list1:
+#         reversed_words.append(word[::-1])
+
+#     return " <--> ".join(reversed_words)
+
+
+# print(reverseSentence("we like coding!"))
+# print(reverseSentence("Burak project"))
