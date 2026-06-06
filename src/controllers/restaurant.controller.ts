8117e62794d6e53c3f1fa50d8d178a563776b075
mrasilbek19 <asilbek190303@gmail.com>
2026-06-06 11:58:40 +0900

    feat: frontend related css, js, and image creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -10,29 +10,31 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
         console.log("Home page")
-        res.send("Home Page");
+        res.render("home");
     } catch (err) {
         console.log("Error, goHome:", err);
-    }
-};
-
-restaurantController.getLogin = (req: Request, res: Response) => {
-    try {
-        console.log("getLogin page")
-        res.send("Login Page");
-    } catch (err) {
-        console.log("Error, getLogin:", err);
     }
 };
 
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
         console.log("getSignup page")
-        res.send("Signup Page");
+        res.render("signup");
     } catch (err) {
         console.log("Error, getSignup:", err);
     }
 };
+
+restaurantController.getLogin = (req: Request, res: Response) => {
+    try {
+        console.log("getLogin page")
+        res.render("login");
+    } catch (err) {
+        console.log("Error, getLogin:", err);
+    }
+};
+
+
 
 restaurantController.processSignup = async (req: Request, res: Response) => {
     try {
