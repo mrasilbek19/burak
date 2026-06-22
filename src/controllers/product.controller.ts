75ab006f6f78f611aa2d68893d0151d2e04ed0dc
mrasilbek19 <asilbek190303@gmail.com>
2026-06-22 22:34:30 +0900

    feat: create new product frontend logic development

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -43,13 +43,13 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
         await productService.createNewProduct(data);
 
         res.send(
-            `<script> alert("Successful creation"); window.location.replace('admin/product/all')</script>`
+            `<script> alert("Successful creation"); window.location.replace('/admin/product/all')</script>`
         );
     } catch (err) {
         console.log("Error, CreateNewProduct:", err);
         const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG
         res.send(
-            `<script> alert("${message}"); window.location.replace('admin/product/all')</script>`
+            `<script> alert("${message}"); window.location.replace('/admin/product/all')</script>`
         );
     }
 };
